da6fa3bedb7aa1d1cb63e4d4fff5053539c593e0
Prof. Chêne <alexandre.duch3ne@gmail.com>
2020-03-25 19:17:02 +0100

    Add vuetify

--- client/main.js
+++ client/main.js
@@ -1,10 +1,15 @@
 import { Meteor } from 'meteor/meteor';
 import Vue from 'vue';
 
+import Vuetify from 'vuetify/dist/vuetify.js';
+import 'vuetify/dist/vuetify.min.css';
+Vue.use(Vuetify);
+
 import App from '../imports/ui/App.vue';
 
 Meteor.startup(() => {
   new Vue({
+    vuetify: new Vuetify(),
     el: '#app',
     ...App,
   });
--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -1,37 +1,10 @@
 <template>
-    <div className="container">
-        <header>
-            <h1>Todo List</h1>
-        </header>
-        <ul>
-            <Task
-                    v-for="task in getTasks()"
-                    v-bind:key="task._id"
-                    v-bind:task="task"
-            />
-        </ul>
-    </div>
+    <v-tabs>
+        <v-tab>Item One</v-tab>
+        <v-tab>Item Two</v-tab>
+        <v-tab>Item Three</v-tab>
+    </v-tabs>
 </template>
 
 <script>
-    import Vue from "vue";
-    import Task from "./Task.vue";
-
-    export default {
-        components: {
-            Task
-        },
-        data() {
-            return {};
-        },
-        methods: {
-            getTasks() {
-                return [
-                    { _id: 1, text: "This is task 1" },
-                    { _id: 2, text: "This is task 2" },
-                    { _id: 3, text: "This is task 3" }
-                ];
-            }
-        }
-    };
 </script>
