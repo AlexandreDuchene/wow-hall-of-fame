020661825557115081fd91d7f67f96c04666946f
Prof. Chêne <alexandre.duch3ne@gmail.com>
2020-05-23 12:54:31 +0200

    Fix cronjobs schedule

--- imports/api/cronjobs/schedule.js
+++ imports/api/cronjobs/schedule.js
@@ -2,7 +2,7 @@ SyncedCron.add({
     name: 'Synchronize external data',
     schedule: function(parser) {
         // parser is a later.parse object
-        return parser.text('at 17:00');
+        return parser.text('at 00:00');
     },
     job: function() {
         import '/bin/syncData.js';
@@ -13,7 +13,7 @@ SyncedCron.add({
     name: 'Compute achievements',
     schedule: function(parser) {
         // parser is a later.parse object
-        return parser.text('at 17:30');
+        return parser.text('at 01:00');
     },
     job: function() {
         import '/bin/computeAchievements.js';
